3a8713e584962899fc8640643f508442281c0d9e
Braden-B <142930171+Braden-B@users.noreply.github.com>
2025-09-22 12:16:15 -0500

    Add files via upload

--- New folder/Main.java
+++ New folder/Main.java
@@ -7,9 +7,10 @@ public class Main {
     public static List<Semaphore> chopsticks = new ArrayList<>();
     public static boolean mutex = false;
     public static Queue<Integer> meals = new LinkedList<>();
+    public static int activePhilosophers = 0;
     public static boolean validateInteger(String testValue, boolean mealFlag){
         if (!testValue.matches("\\d{1,4}")){ // Originally I used a try catch statement with parseInt() but the exception crashed everything. Proud of this solution, as it also limits characters
-            return false;
+            return true;
         }
         if (!mealFlag) {
             return Integer.parseInt(testValue) <= 1;
@@ -20,19 +21,19 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello. How many philosophers should be created? (1-9999): ");
+        System.out.println("Hello. How many philosophers should be created? (2-9999): ");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while(validateInteger(input, false)){
-            System.out.println("Invalid Input. Use a whole number of range 1-9999.");
+            System.out.println("Invalid Input. Use a whole number of range 2-9999.");
             System.out.println("Hello. How many philosophers should be created?: ");
             input = sc.nextLine();
         }
         int numPhilosophers = Integer.parseInt(input);
-        System.out.println("How many meals should they get? (2-9999): ");
+        System.out.println("How many meals should they get? (1-9999): ");
         input = sc.nextLine();
         while(validateInteger(input, true)){
-            System.out.println("Invalid Input. Use a whole number of range 0-9999.");
+            System.out.println("Invalid Input. Use a whole number of range 1-9999.");
             System.out.println("How many meals should they get?: ");
             input = sc.nextLine();
         }
--- New folder/Philosopher.java
+++ New folder/Philosopher.java
@@ -1,3 +1,4 @@
+
 import java.util.Random;
 
 public class Philosopher implements Runnable {
@@ -6,23 +7,33 @@ public class Philosopher implements Runnable {
     private final int leftChopstick;
     private final int rightChopstick;
     private final Random random = new Random();
+    private final int numPhilosophers;
+
     public Philosopher(int position, int numPhilosophers) {
         this.holdingLeftChopstick = false;
         this.holdingRightChopstick = false;
         this.leftChopstick = position;
-        this.rightChopstick = (position+1)%numPhilosophers;
+        this.rightChopstick = (position + 1) % numPhilosophers;
+        this.numPhilosophers = numPhilosophers;
 
     }
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + "Hello! I shall sit at seat number "+ leftChopstick);
-        while (Main.meals.peek() != null) {
-            if (Main.mutex){
-                continue;
+        System.out.println(Thread.currentThread().getName() + "Hello! I shall sit at seat number " + leftChopstick);
+        Main.activePhilosophers++;
+        if (Main.activePhilosophers != numPhilosophers){
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
+        }else {
+            Thread.currentThread().notifyAll();
+        }
+        while (Main.meals.peek() != null) {
             if (holdingRightChopstick && holdingLeftChopstick) {
-                if(Main.mutex){
+                if (Main.mutex) {
                     continue;
                 }
                 Main.mutex = true;
@@ -31,28 +42,33 @@ public class Philosopher implements Runnable {
                 Main.chopsticks.get(rightChopstick).release();
                 holdingRightChopstick = false;
             }
-            Main.mutex = true;
-            holdingLeftChopstick = Main.chopsticks.get(leftChopstick).tryAcquire();
-            if (holdingLeftChopstick) {
-                System.out.println(Thread.currentThread().getName() + "Wonderful! A left chopstick. Now I merely need the right...");
-                holdingRightChopstick = Main.chopsticks.get(rightChopstick).tryAcquire();
-                if (!holdingRightChopstick) {
-                    System.out.println(Thread.currentThread().getName() + "Oh how rude of me. Please, do eat. I shall wait my turn.");
-                    Main.chopsticks.get(leftChopstick).release();
-                    holdingLeftChopstick = false;
-                    Main.mutex = false;
-                }
-            }
-            else{
-                System.out.println(Thread.currentThread().getName() + "Oh how rude of me. Please, do eat. I shall wait my turn.");
+            if (Main.mutex) {
                 continue;
             }
-            Main.mutex = false;
+            else {
+                Main.mutex = true;
+                holdingLeftChopstick = Main.chopsticks.get(leftChopstick).tryAcquire();
+                if (holdingLeftChopstick) {
+                    System.out.println(Thread.currentThread().getName() + "Wonderful! A left chopstick. Now I merely need the right...");
+                    holdingRightChopstick = Main.chopsticks.get(rightChopstick).tryAcquire();
+                    if (!holdingRightChopstick) {
+                        System.out.println(Thread.currentThread().getName() + "Oh how rude of me. Please, do eat. I shall wait my turn.");
+                        Main.chopsticks.get(leftChopstick).release();
+                        holdingLeftChopstick = false;
+                        Main.mutex = false;
+                    }
+                } else {
+                    System.out.println(Thread.currentThread().getName() + "Oh how rude of me. Please, do eat. I shall wait my turn.");
+                    Main.mutex = false;
+                    continue;
+                }
+                Main.mutex = false;
+            }
             if (holdingRightChopstick && holdingLeftChopstick) {
                 System.out.println(Thread.currentThread().getName() + "Two chopsticks! I simply cannot wait to take a bite.");
                 int timer;
                 if (Main.meals.poll() != null) {
-                    System.out.println(Thread.currentThread().getName() +"Ah! A delectable morsel.");
+                    System.out.println(Thread.currentThread().getName() + "Ah! A delectable morsel.");
                     timer = (random.nextInt() % 4) + 3;
                     while (timer > 0) {
                         System.out.println(Thread.currentThread().getName() + "is politely enjoying a bite.");
@@ -72,18 +88,19 @@ public class Philosopher implements Runnable {
                         throw new RuntimeException(e);
                     }
                 }
-                    timer = (random.nextInt() % 4) + 3;
-                    while (timer > 0) {
-                        System.out.println(Thread.currentThread().getName() + "is pondering on the various flavors.");
-                        timer--;
-                    }
+                timer = (random.nextInt() % 4) + 3;
+                Main.mutex = false;
+                while (timer > 0) {
+                    System.out.println(Thread.currentThread().getName() + "is pondering on the various flavors.");
+                    timer--;
                 }
+            }
         }
         try {
+            System.out.println(Thread.currentThread().getName() + "No more meals, a sad occurrence. I shall wait for the rest to finish.");
             Thread.currentThread().join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
-    }
-
+}
